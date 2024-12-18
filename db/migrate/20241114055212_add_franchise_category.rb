e36f1f29d44050a270bf2ed8e6d789166bfaef24
Cinder <cinder@e621.net>
2024-12-18 08:28:40 -0800

    [DB] Fix the migration script name

--- db/migrate/20241114055212_add_franchise_category.rb
+++ db/migrate/20241114055212_add_franchise_category.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class AddContributorCategory < ActiveRecord::Migration[7.1]
+class AddFranchiseCategory < ActiveRecord::Migration[7.1]
   def change
     Post.without_timeout do
       add_column(:posts, :tag_count_franchise, :integer, default: 0, null: false)
