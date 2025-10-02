a9fcf817eca4f693a3b1770a79db6d203a93ed2c
Cinder <cinder@e621.net>
2025-10-02 08:35:50 -0700

    [Misc] Fix a TOS controller test

--- test/functional/terms_of_services_controller_test.rb
+++ test/functional/terms_of_services_controller_test.rb
@@ -8,7 +8,7 @@ class TermsOfServicesControllerTest < ActionDispatch::IntegrationTest
       @admin = create(:admin_user)
       @original_version = Setting.tos_version.to_i
       as(@admin) do
-        create(:wiki_page, title: "e621:terms_of_service", body: "Hello TOS")
+        create(:wiki_page, title: "e6ai:terms_of_service", body: "Hello TOS")
       end
       Cache.clear
     end
